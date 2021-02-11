11fd7edfaabc6aeba99e745c8dfb5b433fe89e05
Erfan Nariman <erfannariman@gmail.com>
2021-02-11 16:19:49 +0100

    fix environment upload

--- azurebatchload/upload.py
+++ azurebatchload/upload.py
@@ -27,9 +27,9 @@ class UploadBatch(Checks):
     def checks(self):
         # check for Azure CLI, credentials and existence dir.
         self._check_azure_cli_installed()
-        self._check_connection_credentials()
+        check_connection_credentials = self._check_connection_credentials()
         self._check_dir()
-        if not self.connection_string:
+        if not self.connection_string and not check_connection_credentials:
             self.connection_string = self._create_connection_string()
 
     def upload(self):
