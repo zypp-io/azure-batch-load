d27a048d4ef6287a83cc44160b9861b5893469d7
Erfan Nariman <erfannariman@gmail.com>
2021-06-03 19:15:35 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fp:
 
 setup(
     name="azurebatchload",
-    version="0.4.1",
+    version="0.4.2",
     author="Erfan Nariman",
     author_email="erfan@zypp.io",
     description="Download files in batches from Azure Blob Storage Containers",
