c5c76e579a1d8570ae173a864fa00bc29e1ce5ac
Erfan Nariman <erfannariman@gmail.com>
2021-08-21 23:37:59 +0200

    fix

--- scripts/check_setupcfg_and_requirementst_equal.py
+++ scripts/check_setupcfg_and_requirementst_equal.py
@@ -1,6 +1,15 @@
 import os
+import pathlib
 
-from scripts.generic import get_config
+from setuptools.config import read_configuration
+
+
+def get_config():
+    repo_path = pathlib.Path(__file__).parent.parent.absolute()
+    config_setup = read_configuration(os.path.join(repo_path, "setup.cfg"))
+    config_requirements = config_setup["options"]["install_requires"]
+
+    return config_requirements, repo_path
 
 
 def check():
