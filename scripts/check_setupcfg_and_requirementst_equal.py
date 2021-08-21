ffb84d58400d8fc0e38fdc057383664c07b4e43d
Erfan Nariman <erfannariman@gmail.com>
2021-08-21 23:13:59 +0200

    get repo path

--- scripts/check_setupcfg_and_requirementst_equal.py
+++ scripts/check_setupcfg_and_requirementst_equal.py
@@ -1,11 +1,16 @@
+import os
+import pathlib
+
 import setuptools.config
 
 
 def check():
-    config_setup = setuptools.config.read_configuration("../setup.cfg")
+    repo_path = pathlib.Path(__file__).parent.parent.absolute()
+    print(repo_path)
+    config_setup = setuptools.config.read_configuration(os.path.join(repo_path, "setup.cfg"))
     requirements_setup = config_setup["options"]["install_requires"]
 
-    with open("../requirements.txt") as f:
+    with open(os.path.join(repo_path, "requirements.txt")) as f:
         requirements_txt = f.read().splitlines()
 
     assert sorted(requirements_setup) == sorted(requirements_txt), "Requirements are not equal"
