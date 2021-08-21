de4633896bc75882af5ce03b3af8e3c14353cf60
Erfan Nariman <erfannariman@gmail.com>
2021-08-21 23:10:44 +0200

    actually call function

--- scripts/check_setupcfg_and_requirementst_equal.py
+++ scripts/check_setupcfg_and_requirementst_equal.py
@@ -8,5 +8,9 @@ def check():
     with open("../requirements.txt") as f:
         requirements_txt = f.read().splitlines()
 
-    assert sorted(requirements_setup) == sorted(requirements_txt)
+    assert sorted(requirements_setup) == sorted(requirements_txt), "Requirements are not equal"
     print("Requirements and setup.cfg and both are equal")
+
+
+if __name__ == "__main__":
+    check()
